ca7e98ad811a18d7d33c57b09300f4197ed0eaf9
edudevjob <eduardotecnologo@hotmail.com.com>
2024-08-14 20:20:48 -0300

    feat: paginate

--- src/main/java/jsfempresas2024/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/jsfempresas2024/erp/controller/GestaoEmpresasBean.java
@@ -5,18 +5,47 @@ import java.io.Serializable;
 import javax.faces.view.ViewScoped;
 import javax.inject.Named;
 
+import jsfempresas2024.erp.model.Empresa;
+import jsfempresas2024.erp.model.TipoEmpresa;
+
 @Named
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable{
 	 private static final long serialVersionUID = 1L;
+	 
+	 private Empresa empresa = new Empresa();
+	 
+	 public void salvar() {
+			System.out.println("Razão social: " + empresa.getRazaoSocial()
+					+ " - " + empresa.getNomeFantasia()
+					+ " - " + empresa.getTipo());
+	}
+	 
+	 public String ajuda() {
+		    return "AjudaGestaoEmpresas?faces-redirect=true";
+		}
+	 
+//	 public String ajuda() {
+//		    System.out.println("Método ajuda() foi chamado");
+//		    return "AjudaGestaoEmpresas"; // Retorna o nome da página (sem a extensão .xhtml)
+//		}
+
+	 
+	 public Empresa getEmpresa() {
+		return empresa;
+	}
+	 
+	 public TipoEmpresa[] getTiposEmpresa() {
+			return TipoEmpresa.values();
+	}
 	
-	 private static Integer NUMERO = 0;
-	 
-	 public GestaoEmpresasBean() {
-		 NUMERO++;
-	 }
-	 
-	 public Integer getNumero() {
-		 return NUMERO;
-	 }
+//	 private static Integer NUMERO = 0;
+//	 
+//	 public GestaoEmpresasBean() {
+//		 NUMERO++;
+//	 }
+//	 
+//	 public Integer getNumero() {
+//		 return NUMERO;
+//	 }
 }
--- src/main/java/jsfempresas2024/erp/model/Empresa.java
+++ src/main/java/jsfempresas2024/erp/model/Empresa.java
@@ -1,6 +1,7 @@
 package jsfempresas2024.erp.model;
 
 import java.io.Serializable;
+import java.math.BigDecimal;
 import java.util.Date;
 
 import javax.persistence.Column;
@@ -45,6 +46,17 @@ public class Empresa implements Serializable{
 	@Enumerated(EnumType.STRING)
 	@Column(nullable = false, length = 30)
 	private TipoEmpresa tipo;
+	
+	@Column(precision = 10, scale = 2)
+    private BigDecimal faturamento;
+
+    public BigDecimal getFaturamento() {
+    	return faturamento;
+    }
+    
+    public void setFaturamento(BigDecimal faturamento) {
+        this.faturamento = faturamento;
+    }
 
 	public Long getId() {
 		return id;
