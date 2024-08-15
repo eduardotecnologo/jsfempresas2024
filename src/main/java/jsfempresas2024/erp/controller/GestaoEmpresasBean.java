81d564f41d17936eb2e68020617c3cc586a46257
edudevjob <eduardotecnologo@hotmail.com.com>
2024-08-14 21:19:03 -0300

    feat: validação de formulário

--- src/main/java/jsfempresas2024/erp/controller/GestaoEmpresasBean.java
+++ src/main/java/jsfempresas2024/erp/controller/GestaoEmpresasBean.java
@@ -11,41 +11,26 @@ import jsfempresas2024.erp.model.TipoEmpresa;
 @Named
 @ViewScoped
 public class GestaoEmpresasBean implements Serializable{
-	 private static final long serialVersionUID = 1L;
-	 
-	 private Empresa empresa = new Empresa();
-	 
-	 public void salvar() {
-			System.out.println("Razão social: " + empresa.getRazaoSocial()
-					+ " - " + empresa.getNomeFantasia()
-					+ " - " + empresa.getTipo());
-	}
-	 
-	 public String ajuda() {
-		    return "AjudaGestaoEmpresas?faces-redirect=true";
-		}
-	 
-//	 public String ajuda() {
-//		    System.out.println("Método ajuda() foi chamado");
-//		    return "AjudaGestaoEmpresas"; // Retorna o nome da página (sem a extensão .xhtml)
-//		}
 
-	 
-	 public Empresa getEmpresa() {
+	private static final long serialVersionUID = 1L;
+
+	private Empresa empresa = new Empresa();
+
+	public void salvar() {
+		System.out.println("Razão social: " + empresa.getRazaoSocial()
+				+ " - " + empresa.getNomeFantasia()
+				+ " - " + empresa.getTipo());
+	}
+
+	public String ajuda() {
+		return "AjudaGestaoEmpresas?faces-redirect=true";
+	}
+
+	public Empresa getEmpresa() {
 		return empresa;
 	}
-	 
-	 public TipoEmpresa[] getTiposEmpresa() {
-			return TipoEmpresa.values();
+
+	public TipoEmpresa[] getTiposEmpresa() {
+		return TipoEmpresa.values();
 	}
-	
-//	 private static Integer NUMERO = 0;
-//	 
-//	 public GestaoEmpresasBean() {
-//		 NUMERO++;
-//	 }
-//	 
-//	 public Integer getNumero() {
-//		 return NUMERO;
-//	 }
 }
